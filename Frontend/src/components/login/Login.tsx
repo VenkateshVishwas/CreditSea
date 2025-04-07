86847deb7eabe673d25c30b989db918ce1e4e906
itw <venkateshvishwas12@gmail.com>
2025-04-07 10:56:05 +0530

    final

---
+++ Frontend/src/components/login/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AuthComponent from "./AuthComponent";
+import "./auth.css";
+
+
+const Login: React.FC = () => {
+  return (
+    <div className="App">
+      <AuthComponent />
+    </div>
+  );
+};
+
+export default Login;
